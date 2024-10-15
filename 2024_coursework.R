80a0dbdf9734ca3f59b32f35e6d4c5880eab3533
CorwinAnsley <coreyansley@gmail.com>
2024-10-15 10:07:55 +0100

    working on comparing neutrophils

--- 2024_coursework.R
+++ 2024_coursework.R
@@ -13,6 +13,9 @@ expression_table = read.table("C:/Users/2266643A/repos/2024_BioStats_Course/Data
 sample_info = read.table("C:/Users/2266643A/repos/2024_BioStats_Course/Data_Report_Assessment/Sample_Information.csv", header=TRUE, row.names=1, sep="\t")
 
 #Are groups well matched 
+sample_info$SAMPLE_GROUP=as.factor(sample_info$SAMPLE_GROUP)
+sample_info$SEX=as.factor(sample_info$SEX)
+
 sample_info_hc = subset(sample_info, sample_info['SAMPLE_GROUP'] == 'HC')
 sample_info_gout = subset(sample_info, sample_info['SAMPLE_GROUP'] == 'GOUT')
 sample_info_sa = subset(sample_info, sample_info['SAMPLE_GROUP'] == 'SEPSIS')
@@ -21,3 +24,6 @@ summary(sample_info_hc)
 summary(sample_info_gout)
 summary(sample_info_sa)
 
+#Plot Neutrophils for each group to compare
+ggp = ggplot(sample_info, aes(x=SAMPLE_GROUP, y=NEUTROPHILS)) + geom_boxplot() + labs(x="Sample Group", y="Sample Group", title="Neutrophils in sample groups")
+ggp
